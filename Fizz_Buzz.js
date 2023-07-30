c7d8f70df57da22ff273addfe55e918dcbf2669b
vikkivignesh <vikki.vishnu04@gmail.com>
2023-07-30 07:41:55 +0530

    testing config

---
+++ Fizz_Buzz.js
@@ -0,0 +1,17 @@
+var i=0;
+for(;i<100;i++)
+{
+    if(i%3==0)
+    {
+        console.log("Fizz");
+    }
+    if(i%5==0)
+    {
+        console.log("Buzz");
+    }
+    if(i%3==0 && i%5==0)
+    {
+        console.log("FizzBuzz");
+    }
+    console.log(i);
+}
