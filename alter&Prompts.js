49f4ad4aa60343de32d9af5386a6b59f7ef8801d
vikkivignesh <vikki.vishnu04@gmail.com>
2023-08-06 20:10:38 +0530

    abt alt&pmpt

---
+++ alter&Prompts.js
@@ -0,0 +1,7 @@
+//Alter is a method which is existing in js,which is used to send warnings in website
+
+alert("Any Text Which we want to display in pop up");
+
+//Prompt is used to take input from the user through pop up window
+
+prompt("ENter any text ------------______________----------")
