5e47f05a7bb8c68e524cfb44659ce642f773bfe2
vikkivignesh <vikki.vishnu04@gmail.com>
2023-08-06 20:19:29 +0530

    R-generator

---
+++ Random.js
@@ -0,0 +1,5 @@
+var rand=Math.random(); //using rand method
+rand=rand*6;  // here 6 is the range, we can give any number till where we want
+//above code will generate the number is decimal formate to ,get int form we should use floor()
+rand=Math.floor(rand)
+console.log(rand)
